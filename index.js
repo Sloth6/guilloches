0d35eb0af63cbfc946ee304dc7c1467ef6480a15
Joel Simon <joelsimon6@gmail.com>
2018-02-10 15:51:05 -0500

    removed Lemniscate scaling and added regualr scaling

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var cos = Math.cos
   , sin = Math.sin
 
-function guilloche(theta, R, r, p, t, f) {
-  R *= Math.sqrt(Math.abs(Math.cos(1*theta)) + t)
-  r *= Math.sqrt(Math.abs(Math.cos(1*theta)) + t)
+function guilloche(theta, scale, R, r, p, t, f) {
+  // R *= Math.sqrt(Math.abs(Math.cos(1*theta)) + t)
+  // r *= Math.sqrt(Math.abs(Math.cos(1*theta)) + t)
 
   var x = (R + r)* Math.cos(theta) + (r+p) * Math.cos(((R+r)/r)*theta)*f
   var y = (R + r)* Math.sin(theta) - (r+p) * Math.sin(((R+r)/r)*theta)*f
-
+  x *= scale
+  y *= scale
   // y *= .5
   // y *= Math.cos(theta) * r2
   // x *= Math.sin(theta) * r2
