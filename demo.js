fd9925536e287a8f0c10fa9b0349263056db9a6a
Joel Simon <joelsimon6@gmail.com>
2018-02-10 15:43:05 -0500

    modifeid guilloche

--- demo.js
+++ demo.js
@@ -1,7 +1,3 @@
-var dat = require('./vendor/dat.gui.js')
-  , raf = require('raf')
-  , hypotrochoid = require('./')
-
 var canvas = document.createElement('canvas')
   , ctx = canvas.getContext('2d')
   , gui = new dat.GUI
@@ -10,106 +6,69 @@ var canvas = document.createElement('canvas')
   , width
   , height
 
-var radii = [
-    Math.random() * 190 + 10
-  , Math.random() * 190 + 10
-  , Math.random() * 190 + 10
-  , Math.random() * 190 + 10
-  , Math.random() * 190 + 10
-  , Math.random() * 190 + 10
-]
 
 var options = {
-    'Slider Count': 3
-  , 'Radius #1': radii[0]
-  , 'Radius #2': radii[1]
-  , 'Radius #3': radii[2]
-  , 'Radius #4': radii[3]
-  , 'Radius #5': radii[4]
-  , 'Radius #6': radii[5]
-  , 'Size': 30 + Math.random() * 60
-  , 'Undulate': true
+  'R': 200
+  , 'r': -0.25
+  , 'p': 10
+  , 'steps': 1000
+  , 't': 1
+  , 'f': 20
 }
 
-gui.add(options, 'Size', 0, 200)
-   .step(1)
-   .onChange(draw)
-
-gui.add(options, 'Slider Count', 2, 6)
-   .step(1)
-   .onFinishChange(updateSliders)
-
-gui.add(options, 'Undulate')
+gui.add(options, 'R', 50, 1000).onChange(draw)
+gui.add(options, 'r', -1, 1).onChange(draw)
+gui.add(options, 'p', 1, 50).onChange(draw)
+gui.add(options, 'steps', 200, 10000).onChange(draw)
+gui.add(options, 't', -.9, 2).onChange(draw)
+gui.add(options, 'f', 0, 50).onChange(draw)
 
 document.body.style.background = '#000'
 document.body.style.margin = '0'
 document.body.style.padding = '0'
 document.body.appendChild(canvas)
 
-function updateSliders(count) {
-  sliders.forEach(function(slider, n) {
-    slider.domElement.parentNode.parentNode.parentNode.parentNode.removeChild(slider.domElement.parentNode.parentNode.parentNode)
-  })
-  sliders = []
-  for (var i = 0; i < count; i += 1) (function(i) {
-    options[key] = radii[i] = radii[i] || 50
-
-    var key = 'Radius #' + (i+1)
-    var slider = gui.add(options, key, 10, 200).onChange(function(value) {
-      radii[i] = value
-      draw()
-    })
-
-    sliders.push(slider)
-  })(i)
-
-  radii.length = count
-  draw()
-}
-updateSliders(options['Slider Count'])
 
 function draw() {
   var h = options['Size']
 
-  ctx.fillStyle = 'black'
+  ctx.fillStyle = 'white'
   ctx.fillRect(0, 0, width, height)
   ctx.save()
   ctx.translate(width/2, height/2)
 
-  ctx.globalCompositeOperation = 'lighter'
-  ctx.strokeStyle = 'rgba(205,255,215,0.25)'
+  // ctx.globalCompositeOperation = 'lighter'
+  // ctx.strokeStyle = 'rgba(205,255,215,0.25)'
+  ctx.strokeStyle = 'black'
   ctx.beginPath()
 
-  // The rest of the code is presentation:
-  // this is how you would use the module
-  // to trace out a curve.
-  hypotrochoid(h, radii, 0, output)
-  ctx.moveTo(output[0], output[1])
-  for (var i = 0; i < 40000; i += 2.5) {
-    hypotrochoid(h, radii, i * Math.PI / 200, output)
-    ctx.lineTo(output[0], output[1])
+  p = guilloche(0, options.R, options.r, options.p, options.t, options.f)
+  ctx.moveTo(p.x, p.y)
+  for (var j = 1; j < options.steps; j++) {
+    theta = (2 * Math.PI) * (j / options.steps)
+    p = guilloche(theta, options.R, options.r, options.p, options.t, options.f)
+    ctx.lineTo(p.x, p.y)
   }
 
   ctx.stroke()
   ctx.restore()
 }
 
-
 function resize() {
   width = canvas.width = window.innerWidth
   height = canvas.height = window.innerHeight
   draw()
 }
-;(window.onresize = resize)()
+(window.onresize = resize)()
 
-raf(canvas).on('data', function() {
-  if (!options.Undulate) return
-  var now = Date.now ? Date.now() : +new Date
+// raf(canvas).on('data', function() {
+//   if (!options.Undulate) return
+//   var now = Date.now ? Date.now() : +new Date
 
-  for (var i = 0, l = radii.length; i < l; i += 1) {
-    radii[i] += Math.sin((now - i * 5000) * 0.0001 * Math.sin(i / 1.3)) * 0.0025
-    options.Size += Math.sin((now + 5000) * 0.00008) * 0.025
-  }
+//   for (var i = 0, l = radii.length; i < l; i += 1) {
+//     radii[i] += Math.sin((now - i * 5000) * 0.0001 * Math.sin(i / 1.3)) * 0.0025
+//     options.Size += Math.sin((now + 5000) * 0.00008) * 0.025
+//   }
 
-  draw()
-})
+//   draw()
+// })
--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 var cos = Math.cos
   , sin = Math.sin
 
-module.exports = hypotrochoid
+function guilloche(theta, R, r, p, t, f) {
+  R *= Math.sqrt(Math.abs(Math.cos(1*theta)) + t)
+  r *= Math.sqrt(Math.abs(Math.cos(1*theta)) + t)
+
+  var x = (R + r)* Math.cos(theta) + (r+p) * Math.cos(((R+r)/r)*theta)*f
+  var y = (R + r)* Math.sin(theta) - (r+p) * Math.sin(((R+r)/r)*theta)*f
+
+  // y *= .5
+  // y *= Math.cos(theta) * r2
+  // x *= Math.sin(theta) * r2
+
+  return {x:y, y:x}
+}
 
 function hypotrochoid(d, radii, t, output) {
   output = output || []
